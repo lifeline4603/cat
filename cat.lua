20d0284f649f4dfdd69ce4dc8f7cce10208e505b
lifeline4603 <135634664+lifeline4603@users.noreply.github.com>
2023-06-11 03:01:23 +0200

    added rotate aa and...
    i added rotate aa in this update so you can make ur aa have a rotation angle. you can customize it with 3 sliders.
    
    i also did some general restructuring to make the code look more... appealing.

--- cat.lua
+++ cat.lua
@@ -51,20 +51,64 @@ if oink.ui_get("cat", "fakelag conditions") then
 hook.Add( "KeyRelease", "PlayerIdle", function( ply, key )
     if oink.ui_get("cat", "fakelag conditions") == true then
         if key == IN_FORWARD or key == IN_BACK or key == IN_MOVELEFT or key == IN_MOVERIGHT then
-        oink.config_set("misc.aa.fakelag", 6,6) -- you can change this
+            oink.config_set("misc.aa.fakelag", 6,6) -- you can change this
 
 
 hook.Add( "KeyPress", "PlayerWalking", function( ply, key )
-if oink.ui_get("cat", "fakelag conditions") == true then
-  if key == IN_FORWARD or key == IN_BACK or key == IN_MOVELEFT or key == IN_MOVERIGHT then
-  oink.config_set("misc.aa.fakelag", 10,10) -- you can change this
+    if oink.ui_get("cat", "fakelag conditions") == true then
+        if key == IN_FORWARD or key == IN_BACK or key == IN_MOVELEFT or key == IN_MOVERIGHT then
+            oink.config_set("misc.aa.fakelag", 10,10) -- you can change this
 
-end
-end
+        end
+    end
 end)
-end
-end
+
+        end
+    end
 end)
+
+        end
+    end
+end)
+
+
+oink.ui_checkbox("rotate AA")
+oink.ui_slider("speed", 1.00, 1000.00, 1)
+oink.ui_slider("minAngle", -180.00, 180.00, 1)
+oink.ui_slider("maxAngle", -180.00, 180.00, 1)
+
+local minValue = oink.ui_get("cat", "minAngle")
+local maxValue = oink.ui_get("cat", "maxAngle")
+local currentValue = minValue
+local increasing = true
+
+local function ChangeFloatValue(changeSpeed)
+    if increasing then
+        currentValue = currentValue + (FrameTime() * changeSpeed)
+        if currentValue >= maxValue then
+            currentValue = maxValue
+            increasing = false
+        end
+    else
+        currentValue = minValue
+        increasing = true
+    end
+
+    oink.config_set("misc.aa.real_yaw.value", currentValue)
 end
+
+local function UpdateFloatValue()
+    if oink.ui_get("cat", "rotate AA") then
+        local changeSpeed = oink.ui_get("cat", "speed")
+
+        ChangeFloatValue(changeSpeed)
+    end
 end
+
+oink.event_remove("view_render_post", "cat_rotateAA")
+oink.event_listen("view_render_post", "cat_rotateAA", function()
+    minValue = oink.ui_get("cat", "minAngle")
+    maxValue = oink.ui_get("cat", "maxAngle")
+
+    hook.Add("Think", "UpdateFloatValue", UpdateFloatValue)
 end)
